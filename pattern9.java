fe298dad72f41a0466a968c223c590d17b33d405
Gurmeet Kaur <131999481+kgurmeet@users.noreply.github.com>
2023-09-01 16:00:04 +0530

    Create pattern9.java

---
+++ pattern9.java
@@ -0,0 +1,16 @@
+
+public class pattern9{
+    public static void main(String[] args) {
+        int k=6,y=6;
+        for(k=6;k>=1;k--)
+        {
+            for(y=k;y>=1;y--)
+            {
+                System.out.print(y);
+            }
+            System.out.println();
+        }
+    }
+    
+}
+
